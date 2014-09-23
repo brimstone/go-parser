fb0db4dbbf3a02a04654fbe982cba70224cc2b3e
Matt <brimstone@the.narro.ws>
2014-09-23 12:44:21 -0400

    Moving to levels instead so I have some order of operations

--- parser.go
+++ parser.go
@@ -44,105 +44,109 @@ func Parse(env Env, input string) (bool, error) {
 	}
 	// Attempt to loop through our tokens until we can reduce our tokens to 1
 
-	clean := false
+	level := 2
 TokenCheck:
-	for clean != true && len(tokens) > 1 {
-		clean = true
+	for level > 0 && len(tokens) > 1 {
 		for i, _ := range tokens {
-			// check dualnary operators
-			if i > 0 && len(tokens) > i+1 {
-				left := resolveEnv(env, tokens[i-1])
-				right := resolveEnv(env, tokens[i+1])
-				switch tokens[i] {
-				case "=":
-					// do the op
-					// this also covers bools and ints since they're both strings
-					// FALSE != false however
-					if left == right {
-						tokens[i-1] = "true"
-					} else {
-						tokens[i-1] = "false"
-					}
-					clean = false
-					tokens = append(tokens[:i], tokens[i+2:]...)
-					break TokenCheck
-				case "<":
-					// attempt to convert left to an int
-					leftInt, err := strconv.Atoi(left)
-					if err != nil {
-						return false, fmt.Errorf("Expect type int, found", left)
-					}
-					// attempt to convert right to an int
-					rightInt, err := strconv.Atoi(right)
-					if err != nil {
-						return false, fmt.Errorf("Expect type int, found", right)
-					}
-					// do the op
-					if leftInt < rightInt {
-						tokens[i-1] = "true"
-					} else {
-						tokens[i-1] = "false"
-					}
-					clean = false
-					tokens = append(tokens[:i], tokens[i+2:]...)
-					break TokenCheck
-				case ">":
-					// attempt to convert left to an int
-					leftInt, err := strconv.Atoi(left)
-					if err != nil {
-						return false, fmt.Errorf("Expect type int, found", left)
-					}
-					// attempt to convert right to an int
-					rightInt, err := strconv.Atoi(right)
-					if err != nil {
-						return false, fmt.Errorf("Expect type int, found", right)
-					}
-					// do the op
-					if leftInt > rightInt {
-						tokens[i-1] = "true"
-					} else {
-						tokens[i-1] = "false"
-					}
-					clean = false
-					tokens = append(tokens[:i], tokens[i+2:]...)
-					break TokenCheck
-				case "|":
-					if left == "true" {
-						clean = false
-						tokens[i-1] = "true"
+			switch level {
+			case 2:
+				// check dualnary operators
+				if i > 0 && len(tokens) > i+1 {
+					left := resolveEnv(env, tokens[i-1])
+					right := resolveEnv(env, tokens[i+1])
+					switch tokens[i] {
+					case "<":
+						// attempt to convert left to an int
+						leftInt, err := strconv.Atoi(left)
+						if err != nil {
+							return false, fmt.Errorf("Expect type int, found", left)
+						}
+						// attempt to convert right to an int
+						rightInt, err := strconv.Atoi(right)
+						if err != nil {
+							return false, fmt.Errorf("Expect type int, found", right)
+						}
+						// do the op
+						if leftInt < rightInt {
+							tokens[i-1] = "true"
+						} else {
+							tokens[i-1] = "false"
+						}
 						tokens = append(tokens[:i], tokens[i+2:]...)
 						break TokenCheck
-					} else if left != "false" {
-						return false, fmt.Errorf("Expected 'true' or 'false', found", left)
-					}
-					if right == "true" {
-						clean = false
-						tokens[i-1] = "true"
-						tokens = append(tokens[:i], tokens[i+2:]...)
-						break TokenCheck
-					} else if right != "false" {
-						return false, fmt.Errorf("Expected 'true' or 'false', found", right)
-					}
-				case "&":
-					if left == "true" && right == "true" {
-						clean = false
-						tokens[i-1] = "true"
+					case ">":
+						// attempt to convert left to an int
+						leftInt, err := strconv.Atoi(left)
+						if err != nil {
+							return false, fmt.Errorf("Expect type int, found", left)
+						}
+						// attempt to convert right to an int
+						rightInt, err := strconv.Atoi(right)
+						if err != nil {
+							return false, fmt.Errorf("Expect type int, found", right)
+						}
+						// do the op
+						if leftInt > rightInt {
+							tokens[i-1] = "true"
+						} else {
+							tokens[i-1] = "false"
+						}
 						tokens = append(tokens[:i], tokens[i+2:]...)
 						break TokenCheck
 					}
-					if left != "false" && left != "true" {
-						return false, fmt.Errorf("Expected 'true' or 'false', found '%s'", left)
+				}
+			case 1:
+				// check dualnary operators
+				if i > 0 && len(tokens) > i+1 {
+					left := resolveEnv(env, tokens[i-1])
+					right := resolveEnv(env, tokens[i+1])
+					switch tokens[i] {
+					case "=":
+						// do the op
+						// this also covers bools and ints since they're both strings
+						// FALSE != false however
+						if left == right {
+							tokens[i-1] = "true"
+						} else {
+							tokens[i-1] = "false"
+						}
+						tokens = append(tokens[:i], tokens[i+2:]...)
+						break TokenCheck
+					case "|":
+						if left == "true" {
+							tokens[i-1] = "true"
+							tokens = append(tokens[:i], tokens[i+2:]...)
+							break TokenCheck
+						} else if left != "false" {
+							return false, fmt.Errorf("Expected 'true' or 'false', found", left)
+						}
+						if right == "true" {
+							tokens[i-1] = "true"
+							tokens = append(tokens[:i], tokens[i+2:]...)
+							break TokenCheck
+						} else if right != "false" {
+							return false, fmt.Errorf("Expected 'true' or 'false', found", right)
+						}
+					case "&":
+						if left == "true" && right == "true" {
+							tokens[i-1] = "true"
+							tokens = append(tokens[:i], tokens[i+2:]...)
+							break TokenCheck
+						}
+						if left != "false" && left != "true" {
+							return false, fmt.Errorf("Expected 'true' or 'false', found '%s'", left)
+						}
+						if right != "false" && right != "true" {
+							return false, fmt.Errorf("Expected 'true' or 'false', found '%s'", right)
+						}
+						tokens[i-1] = "false"
+						tokens = append(tokens[:i], tokens[i+2:]...)
+						break TokenCheck
 					}
-					if right != "false" && right != "true" {
-						return false, fmt.Errorf("Expected 'true' or 'false', found '%s'", right)
-					}
-					clean = false
-					tokens[i-1] = "false"
-					tokens = append(tokens[:i], tokens[i+2:]...)
-					break TokenCheck
 				}
 			}
 		}
+		level--
 	}
 
 	// Now that we've reduced our tokens to only one element,
@@ -156,7 +160,6 @@ TokenCheck:
 				tokens[0] = "true"
 			}
 		case bool:
-			fmt.Println("it's a bool")
 			// maybe we should keep it in the determined form?
 			if t {
 				tokens[0] = "true"
